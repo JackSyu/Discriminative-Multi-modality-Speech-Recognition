8ba4a875ddaca324a327012be72d45b96f1790aa
JackSyu <37199018+JackSyu@users.noreply.github.com>
2020-06-12 20:50:17 +0800

    Update Test_AE_MSR.py

--- Test_AE_MSR.py
+++ Test_AE_MSR.py
@@ -9,28 +9,21 @@ from configuration import ModelConfig, TrainingConfig
 import numpy as np
 from statistic import cer_s
 
-
 FLAGS = tf.app.flags.FLAGS
 
-tf.flags.DEFINE_string('vocab_path', '/home/jack/model/w22lmc/original_english_word_count.txt', 'dictionary path')
-
-tf.flags.DEFINE_integer('NUM_EPOCH', 100, 'epoch次数')
-
+tf.flags.DEFINE_string('vocab_path', '/home/jack/model/original_english_word_count.txt', 'dictionary path')
 
 def main(unused_argv):
 
     vocab = Vocabulary(FLAGS.vocab_path)
-    #vocab.id_to_word['-1'] = -1
 
-    model_dir = 'AVSR_LAB_transformer_1word' + datetime.datetime.now().strftime('%Y:%m:%d:%H:%M:%S')
+    model_path = '/data-private/nas/src/Multi_Model/AE_MSR2019:10:02:01:29:18/ckp30'
     model_name = 'ckp'
     model_config = ModelConfig()
     train_config = TrainingConfig()
 
-    train_dataset = build_dataset(model_config.train_tfrecord_list, batch_size=model_config.batch_size, shuffle=True)
     val_dataset = build_dataset(model_config.val_tfrecord_list, batch_size=model_config.batch_size, is_training=False)
     iterator = tf.data.Iterator.from_structure(train_dataset.output_types, train_dataset.output_shapes)
-    train_init_op = iterator.make_initializer(train_dataset)
     val_init_op = iterator.make_initializer(val_dataset)
 
     config = tf.ConfigProto()
@@ -43,62 +36,40 @@ def main(unused_argv):
 
     sess = tf.Session(config=config)
     sess.run(tf.global_variables_initializer())
-
-    saver = tf.train.Saver(max_to_keep=FLAGS.NUM_EPOCH)
-
-    summary_writer = tf.summary.FileWriter('logs_no_mh/asr_transformer' +
-                                                    datetime.datetime.now().strftime('%Y:%m:%d:%H:%M:%S'),
-                                                    graph=sess.graph)
+    vL = tf.trainable_variables()
+    r_saver = tf.train.Saver(var_list=vL, max_to_keep=FLAGS.NUM_EPOCH)
+    r_saver.restore(sess, model_path)
 
     print('Model compiled')
 
-    count = 0
-    for epoch in range(FLAGS.NUM_EPOCH):
-
-        print('Epoch %d] eval begin' % epoch)
-        val_total_loss = 0
-        sess.run(val_init_op)
-        val_pairs = []
-        i = 0
-        if epoch > -1:
-            # for jjjj in range(118550):
-            while True:
-                try:
-                    out_indices, loss1, y = model.eval(sess)
-                    print('pred: ', out_indices)
-                    print('ground truth: ', y)
-                    print('loss: ', loss1)
-                    val_total_loss += loss1
-                    print('\n   [%d ]' % (i))
-                    for j in range(len(y)):
-                        unpadded_out = None
-                        if 1 in out_indices[j]:
-                            idx_1 = np.where(out_indices[j] == 1)[0][0]
-                            unpadded_out = out_indices[j][:idx_1]
-                        else:
-                            unpadded_out = out_indices[j]
-                        idx_1 = np.where(y[j] == 1)[0][0]
-                        unpadded_y = y[j][:idx_1]
-                        predic = ''.join([vocab.id_to_word[k] for k in unpadded_out])
-                        label = ''.join([vocab.id_to_word[i] for i in unpadded_y])
-                        val_pairs.append((predic, label))
-                    i += 1
-                except:
-                    break
-            avg_loss = val_total_loss / max(i, 1)
-            print("avg_loss", avg_loss)
-            counts, cer = cer_s(val_pairs)
-
-            summary = tf.Summary(value=[tf.Summary.Value(tag="cer", simple_value=cer),
-                                        tf.Summary.Value(tag="val_loss", simple_value=avg_loss)])
-            summary_writer.add_summary(summary, epoch)
-
-            print('Current error rate is : %.4f' % cer)
-        print('Epoch %d] eval end' % epoch)
-
+    sess.run(val_init_op)
+    val_pairs = []
+    while True:
+        try:
+        out_indices, loss1, y = model.eval(sess)
+        print('pred: ', out_indices)
+        print('ground truth: ', y)
+        print('loss: ', loss1)
+        val_total_loss += loss1
+        print('\n   [%d ]' % (i))
+        for j in range(len(y)):
+            unpadded_out = None
+            if 1 in out_indices[j]:
+                idx_1 = np.where(out_indices[j] == 1)[0][0]
+                unpadded_out = out_indices[j][:idx_1]
+            else:
+                unpadded_out = out_indices[j]
+                idx_1 = np.where(y[j] == 1)[0][0]
+                unpadded_y = y[j][:idx_1]
+                predic = ''.join([vocab.id_to_word[k] for k in unpadded_out])
+                label = ''.join([vocab.id_to_word[i] for i in unpadded_y])
+                val_pairs.append((predic, label))
+        except:
+            break
+        counts, cer = cer_s(val_pairs)
+        print('Current error rate is : %.4f' % cer) 
+        
         #############################################################
-
-    summary_writer.close()
 
 
 if __name__ == '__main__':
